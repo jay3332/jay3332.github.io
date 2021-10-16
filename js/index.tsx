6d452c334ddabbbbfa5d436f0a0342d55316d5eb
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-10-16 15:56:22 -0400

    Fix assets?

--- js/index.tsx
+++ js/index.tsx
@@ -4,8 +4,8 @@ import * as React from 'react';  // Fixes linter errors
 
 ReactDOM.render(
     <span>
-        <BannerButton href='https://github.com/jay3332' alt='Github' icon='./assets/github.svg' />
-        <BannerButton href='#' alt='Discord' icon='./assets/discord.svg' />
+        <BannerButton href='https://github.com/jay3332' alt='Github' icon='/assets/github.svg' />
+        <BannerButton href='#' alt='Discord' icon='/assets/discord.svg' />
     </span>,
     document.getElementById('banner-buttons')
 );
