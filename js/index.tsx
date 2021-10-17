0378debf5c8e4241e90ed75aac89b2dadc892cd4
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-10-16 22:13:27 -0400

    Final touches?

--- js/index.tsx
+++ js/index.tsx
@@ -1,11 +1,11 @@
-import BannerButton from './components/banner-button';
+import BannerButton from './components/BannerButton';
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';  // Fixes linter errors
 
 ReactDOM.render(
     <span>
         <BannerButton href='https://github.com/jay3332' alt='Github' icon='./assets/github.svg' />
-        <BannerButton href='#' alt='Discord' icon='./assets/discord.svg' />
+        <BannerButton href='https://discord.gg/FqtZ6akWpd' alt='Discord' icon='./assets/discord.svg' />
     </span>,
     document.getElementById('banner-buttons')
 );
