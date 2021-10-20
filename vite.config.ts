fdef934d1c9712b3191183ac05212af34247cb5e
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-10-20 15:04:11 +0000

    Small styling tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+    plugins: [reactRefresh()],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            port: 443,
+        },
+    }
 })
